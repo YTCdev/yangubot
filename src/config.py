4c98eedd6ef3e5354e01535e56922f71cbbe8eea
unknown <thewildtree@outlook.com>
2020-07-26 16:37:53 +0200

    Add YTC channel IDs to config

--- src/config.py
+++ src/config.py
@@ -10,6 +10,12 @@ class Config:
     OWNER_IDS = []
     ROLE_IDS = []
 
+    CHANNELS = {
+        'support': 210588354096529416,
+        'lounge': 210828828300279808,
+        'gallery': 212328069082513408
+    }
+
     COUNTRIES = ['CA', 'US', 'AU', 'AT', 'BE', 'BG', 'CY', 'CZ', 'DK', 'EE',
                  'FI', 'FR', 'DE', 'GR', 'HU', 'HR', 'IE', 'IT', 'LV', 'LT',
                  'LU', 'MT', 'NL', 'PL', 'PT', 'RO', 'SK', 'SI', 'ES', 'SE',
