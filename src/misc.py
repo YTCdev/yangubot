a522379b31a6e8cc78f4428eebdc4e5bfd96a3a9
celery <ceIery@users.noreply.github.com>
2020-10-25 16:16:47 -0400

    Implement Misc.is_owner

--- src/misc.py
+++ src/misc.py
@@ -9,6 +9,10 @@ class Misc:
         return any(id in roles for id in Config.STAFF_IDS)
 
     @staticmethod
+    def is_owner(ctx):
+        return ctx.author.roles in Config.STAFF_IDS
+
+    @staticmethod
     async def send_error(ctx, message, footer=None):
         embed = discord.Embed(title=message, colour=Config.COLOURS['failed'])
         if footer is not None:
