808a81e1e279ccfc7c77ea4a32aa62c6569d5dc8
celery <ceIery@users.noreply.github.com>
2020-10-25 18:49:05 -0400

    Added some documentation to config_ex.py

--- src/config_ex.py
+++ src/config_ex.py
@@ -2,26 +2,42 @@ from discord import Colour
 
 
 class Config:
+    # WooCommerce API settings
+    #
+    # Get an API key by going to WooCommerce > Settings > Advanced > REST API on
+    # your WordPress installation.
     WCM_URL = ''
     WCM_KEY = ''
     WCM_SECRET = ''
 
+    # Discord API settings
+    #
+    # Get your bot token at https://discord.com/developers/applications
     BOT_TOKEN = ''
-    OWNER_IDS = []
-    STAFF_IDS = []
-    PATRON_ID = 0
 
+    # Discord server settings
+    # -----------------------
+    # The user IDs of the owners of the bot
+    OWNER_IDS = []
+    # The role IDs of the staff role(s) on the server
+    STAFF_IDS = []
+    # The role to give users if they have purchased a product
+    # (set to 0 if none)
+    PATRON_ID = 0
+    # The text channels designated to customer support, general discussion, and
+    # a product gallery, respectively (set to 0 if none)
     CHANNELS = {
         'support': 0,
         'lounge': 0,
         'gallery': 0
     }
-
+    # List of countries with faster shipping
+    # TODO: WooCommerce integration to calculate shipping estimates
     COUNTRIES = ['CA', 'US', 'AU', 'AT', 'BE', 'BG', 'CY', 'CZ', 'DK', 'EE',
                  'FI', 'FR', 'DE', 'GR', 'HU', 'HR', 'IE', 'IT', 'LV', 'LT',
                  'LU', 'MT', 'NL', 'PL', 'PT', 'RO', 'SK', 'SI', 'ES', 'SE',
                  'GB']
-
+    # Colours to use for each order status
     COLOURS = {
         'completed': Colour(0x4caf50),
         'cancelled': Colour(0xf44336),
